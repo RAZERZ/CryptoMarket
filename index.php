3661cb216beeceb751615f2d2846b1c5e07c9ac4
Rami <ramiabouzahra@gmail.com>
2017-11-09 16:38:18 +0100

    Added more Coin Prices

--- index.php
+++ index.php
@@ -18,6 +18,12 @@
         </center>
         
         <table class="Product-Cat" align="left">
+            <tr>
+                <th class="ProductCats">Product Categories</th>
+            </tr>
+            <tr>
+                <td class="ProductList"></td>
+            </tr>
         </table>
         
         <table class="Currency-Info" align="right">
@@ -30,12 +36,25 @@
                 <td class="Currency-TD">
             <?php
             
-            $json_file = file_get_contents('https://blockchain.info/ticker'); //Sets the json file into a variable
-            $json_decode = json_decode($json_file, true); //Decodes the variable and stores the information in an array
+            $json_file_BTC = file_get_contents('https://blockchain.info/ticker'); //Sets the json file into a variable
+            $json_decode_BTC = json_decode($json_file_BTC, true); //Decodes the variable and stores the information in an array
             
-            echo($json_decode['USD']['symbol']) . ($json_decode['USD']['last']); //Prints out from section USD the 'last' and the 'symbol values'
+            echo($json_decode_BTC['USD']['symbol']) . ($json_decode_BTC['USD']['last']); //Prints out from section USD the 'last' and the 'symbol values'
             
             ?> 
+                </td>
+            </tr>
+            <tr>
+                <td class="Currency-TD">BCH</td>
+                <td class="Currency-TD">
+                    <?php
+                    
+                    $json_file_BCH = file_get_contents('https://api.cryptonator.com/api/ticker/bch-usd');
+                    $json_decode_BCH = json_decode($json_file_BCH, true);
+                    
+                    echo "$" . ($json_decode_BCH['ticker']['price']);
+                    
+                    ?>
                 </td>
             </tr>
             <tr>
@@ -43,12 +62,51 @@
                 <td class="Currency-TD">
             <?php
 
-            $json_file = file_get_contents('https://api.kraken.com/0/public/Ticker?pair=ETHUSD');
-            $json_decode = json_decode($json_file, true);
+            $json_file_ETH = file_get_contents('https://api.kraken.com/0/public/Ticker?pair=ETHUSD');
+            $json_decode_ETH = json_decode($json_file_ETH, true);
 
-            echo "$" . ($json_decode['result']['XETHZUSD']['a']['0']);
+            echo "$" . ($json_decode_ETH['result']['XETHZUSD']['a']['0']);
 
             ?>
+                </td>
+            </tr>
+            <tr>
+                <td class="Currency-TD">ETC</td>
+                <td class="Currency-TD">
+                    <?php
+                    
+                    $json_file_ETC = file_get_contents('https://api.cryptonator.com/api/ticker/etc-usd');
+                    $json_decode_ETC = json_decode($json_file_ETC, true);
+                    
+                    echo "$" . ($json_decode_ETC['ticker']['price']);
+                    
+                    ?>
+                </td>
+            </tr>
+            <tr>
+                <td class="Currency-TD">DASH</td>
+                <td class="Currency-TD">
+                    <?php
+                    
+                    $json_file_DASH = file_get_contents('https://www.cryptonator.com/api/ticker/dash-usd');
+                    $json_decode_DASH = json_decode($json_file_DASH, true);
+                    
+                    echo "$" . ($json_decode_DASH['ticker']['price']);
+                    
+                    ?>
+                </td>
+            </tr>
+            <tr>
+                <td class="Currency-TD">LTC</td>
+                <td class="Currency-TD">
+                    <?php
+                    
+                    $json_file_LTC = file_get_contents('https://api.cryptonator.com/api/ticker/ltc-usd');
+                    $json_decode_LTC = json_decode($json_file_LTC, true);
+                    
+                    echo "$" . ($json_decode_LTC['ticker']['price']);
+                    
+                    ?>
                 </td>
             </tr>
         </table>
